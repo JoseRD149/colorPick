d0a6c1a52774d5a756befb20e69857c764678bab
JoseRD149 <joseromerodiaz89@gmail.com>
2025-01-23 23:34:38 +0100

    colorpick

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,47 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [color, setColor] = useState('#48fea9');
 
+  const handleColorChange = (e) => {
+    setColor(e.target.value); 
+  };
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Color Picker</h1>
+        <div 
+          style={{
+            backgroundColor: color,  
+            width: '300px',           
+            height: '300px',          
+            marginTop: '20px',        
+            border: '2px solid lightgray', 
+            borderRadius: '40px',     
+            display: 'flex',          
+            flexDirection: 'column',  
+            alignItems: 'center',     
+            justifyContent: 'center'  
+          }}
+        >
+          <h2>Selected Color</h2>
+  
+          <h3>{color}</h3>
+        </div>
+  
+        <h2>Select a color</h2>
+        <input 
+          type="color" 
+          value={color} 
+          onChange={handleColorChange} 
+          style={{ width: '70px', height: '40px', borderRadius: '5px', border: '2px solid lightgray'}} 
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+  
+  
+  
 }
 
 export default App
